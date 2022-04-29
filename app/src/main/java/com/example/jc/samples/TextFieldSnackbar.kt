1ab9b1ee7a562721a82829a750b27580d415583e
eduVieiraAF <edu_vieira@hotmail.com>
2022-04-29 13:49:56 -0300

    Hiding keyboard in textField

--- app/src/main/java/com/example/jc/samples/TextFieldSnackbar.kt
+++ app/src/main/java/com/example/jc/samples/TextFieldSnackbar.kt
@@ -1,21 +1,29 @@
 package com.example.jc.samples
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jc.ui.theme.JCTheme
 import kotlinx.coroutines.launch
 
 
+
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun ClickText(){
     val scaffoldState = rememberScaffoldState()
     var textFieldState by remember { mutableStateOf("") }
     val scope = rememberCoroutineScope()
+    val keyboardController = LocalSoftwareKeyboardController.current
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
@@ -28,12 +36,15 @@ fun ClickText(){
                 .fillMaxSize()
                 .padding(horizontal = 30.dp)
         ) {
+
             TextField(
                 value = textFieldState,
                 label = { Text("Enter your name") },
                 onValueChange = { textFieldState = it },
                 singleLine = true,
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(onDone = { keyboardController?.hide() })
             )
 
             Spacer(modifier = Modifier.height(12.dp))
